67f23a4fb6f87ca3f29214077d4b027280ab6515
KripeshM <mkripeshmullachery@gmail.com>
2023-03-21 17:29:33 +0530

    cms

---
+++ src/app/contact-manager/contact-manager.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { MyContact } from 'src/model/myContact';
+import { ApiService } from '../services/api.service';
+
+@Component({
+  selector: 'app-contact-manager',
+  templateUrl: './contact-manager.component.html',
+  styleUrls: ['./contact-manager.component.css']
+})
+export class ContactManagerComponent implements OnInit {
+//display system date and time
+  loginDate:any;
+
+  //searching
+  searchKey:string='';
+
+  // heading='Contact Details'
+
+    // allContacts:any
+
+  //string interpolation
+    allContacts:MyContact[]=[] //empty array
+
+  constructor(private api:ApiService){
+    this.loginDate=new Date()
+  }
+
+  ngOnInit():void{
+    this.getAllContacts() 
+  }
+  
+  getAllContacts(){
+    this.api.getAllContacts().subscribe((data:any)=>{
+      console.log(data);//array(5) all contact details
+      this.allContacts=data;
+      
+    })
+
+  }
+
+  // nameChange(){
+  //   alert('Value changed');
+  // }
+
+  search(event:any){
+    console.log(event.target.value);
+    this.searchKey=event.target.value;
+  }
+
+  deleteContact(contactId:any){
+    this.api.deleteContact(contactId).subscribe((data:any)=>{
+      alert("Contact deleted");
+      this.getAllContacts()
+    })
+  }
+  
+
+}
